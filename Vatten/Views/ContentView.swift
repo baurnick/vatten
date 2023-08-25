14ab43562aded5518cd8f3c8caacd92bb45f03b9
Nick Baur <nick.baur@students.fhnw.ch>
2023-08-25 12:39:27 +0200

    feat: add auth view

--- Vatten/Views/ContentView.swift
+++ Vatten/Views/ContentView.swift
@@ -9,14 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.background
-                .ignoresSafeArea()
-            
-            Text("Hoi Jessi")
-                .font(.largeTitle)
-                .bold()
-                .foregroundColor(Color.main)
+        NavigationStack {
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("VATTEN")
+                            .font(.custom("IBM", size: 32))
+                        .tracking(10)
+                        
+                        Text("Vergiss mit der Vatten App nie mehr deine Pflanzen zu giessen!")
+                            .font(.system(size: 24))
+                            .fontWeight(.thin)
+                    }
+                    Spacer()
+                }
+                .padding()
+                
+                Image("Vatten_plants")
+                    .resizable()
+                    .scaledToFit()
+                
+                Spacer()
+                
+                
+                NavigationLink {
+                    // TBD
+                } label: {
+                    Text("Registrieren")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 18))
+                        .bold()
+                        .frame(maxWidth: .infinity, maxHeight: 50)
+                        .background(RoundedRectangle(cornerRadius: 12))
+                        .tint(Color.main)
+                }
+                .padding(.horizontal)
+                
+                
+                NavigationLink {
+                    // TBD
+                } label: {
+                    Text("Anmelden")
+                        .foregroundColor(Color.main)
+                        .font(.system(size: 18))
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                }
+                .buttonStyle(.plain)
+                .padding([.horizontal, .bottom])
+            }
+            .padding(.top, 48)
         }
     }
 }
